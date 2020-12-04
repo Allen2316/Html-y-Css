2f76f379e64c35f2fb54f2b4b97f4231fe062f4c
Luis Alberto Sarmiento Cuenca <52260850+Allen2316@users.noreply.github.com>
2020-12-04 13:48:51 -0500

    update
    practica de jquery con bases de datos

---
+++ practicas_ajax/Inserta_Jquery/insertar.php
@@ -0,0 +1,10 @@
+<?php
+   	$conexion=mysqli_connect('localhost','root','1234','insertar'); 
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+
+    $sql = "insert into usuarios (nombre, apellido, usuario, password) 
+            values ('$nombre','$apellido','$usuario', '$password')";
+    echo mysqli_query($conexion,$sql);
