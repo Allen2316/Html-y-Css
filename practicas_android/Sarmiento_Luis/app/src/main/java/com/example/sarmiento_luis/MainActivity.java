c90ba1a1079b5c0e75ea7eb3d73e9c7477316e35
Luis Alberto Sarmiento Cuenca <52260850+Allen2316@users.noreply.github.com>
2021-02-11 14:54:57 -0500

    update
    Correccion de errores y avance

--- practicas_android/Sarmiento_Luis/app/src/main/java/com/example/sarmiento_luis/MainActivity.java
+++ practicas_android/Sarmiento_Luis/app/src/main/java/com/example/sarmiento_luis/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.sarmiento_luis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sarmiento_luis.ui.login.ActivityLogin;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +36,48 @@ public class MainActivity extends AppCompatActivity {
                 bundle.putString("apellido", cajaApellidos.getText().toString());
                 intent.putExtras(bundle);
                 startActivity(intent);
-
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.opcionLogin:
+                intent = new Intent(MainActivity.this, ActivityLogin.class);
+                startActivity(intent);
+                break;
+            case R.id.opcionFragmentos:
+                intent = new Intent(MainActivity.this, ActividadFragmentos.class);
+                startActivity(intent);
+                break;
+            case R.id.opcionDialogo:
+                Dialog dialogoLogin = new Dialog(MainActivity.this);
+                dialogoLogin.setContentView(R.layout.dlg_login);
+                EditText cajaUsuario = dialogoLogin.findViewById(R.id.txtUsuarioDlg);
+                EditText cajaClave = dialogoLogin.findViewById(R.id.txtClaveDlg);
+                Button btnDlg = dialogoLogin.findViewById(R.id.btnIngresarDlg);
+                btnDlg.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,
+                                cajaUsuario.getText().toString() + " "
+                                        + cajaClave.getText().toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialogoLogin.show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
