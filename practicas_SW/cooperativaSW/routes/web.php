325f1d2de441168dfeb95c1de63d3b5771c7038e
Luis Alberto Sarmiento Cuenca <52260850+Allen2316@users.noreply.github.com>
2021-02-22 21:06:31 -0500

    update
    correccion de errores en metodos de agregar usando api_token

--- practicas_SW/cooperativaSW/routes/web.php
+++ practicas_SW/cooperativaSW/routes/web.php
@@ -17,21 +17,23 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(["prefix" => "cliente"], function ($router) {
-    $router->get("all", "ClienteController@allSinRestricciones");    
-    $router->get("allJson", "ClienteController@allJson");
-    $router->get("get/{cedula}", "ClienteController@getCliente");
-    /* $router->get('new', 'ClienteController@create'); */
+/* $router->group(['middleware' => 'auth'], function () use ($router) { */
+    $router->group(["prefix" => "cliente"], function ($router) {
+        $router->get("all", "ClienteController@allSinRestricciones");
+        $router->get("allJson", "ClienteController@allJson");
+        $router->get("get/{cedula}", "ClienteController@getCliente");
+        /* $router->post('new', 'ClienteController@create'); */
+    });
+/* }); */
+
+$router->group(['prefix' => 'usuario'], function ($router) {
+    $router->post('ingresar', 'UserController@login');
+    $router->post('salir', 'UserController@logout');
 });
 
-$router->group(['prefix' => 'usuario'], function($router){
-    $router->post('ingresar', 'UserController@login'); 
-    $router->post('salir', 'UserController@logout'); 
-});
 
-
-$router->group(['middleware' => 'auth'], function($router){
-    $router->group(["prefix" => "cliente"], function ($router) {       
-        $router->get('new', 'ClienteController@create');
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->group(["prefix" => "cliente"], function ($router) {
+        $router->post('new', 'ClienteController@create');
     });
 });
